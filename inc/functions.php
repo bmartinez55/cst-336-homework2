a3d151743f0eaa1d3f2ada52bca4094b9cfdb390
Brian Martinez <bmartinez-castaneda@csumb.edu>
2018-02-18 20:07:10 +0000

    First Commit

---
+++ inc/functions.php
@@ -0,0 +1,30 @@
+<?php
+    function Go(){
+        $songs=array("https://www.youtube.com/embed/ISbnf9qCmhI?autoplay=1","https://www.youtube.com/embed/CSD2J8yaMmM?autoplay=1","https://www.youtube.com/embed/N9XoGYgCVzg?autoplay=1");
+        $album=array("FridayAlbum","Stankonia","MikeJones");
+        
+        $randIndex = rand(0,2);
+        
+        $song = $songs[$randIndex];
+        $cover = $album[$randIndex];
+        
+        foreach($songs as $s){
+            if(count($songs)<0 or count($cover)<0){
+                return false;
+            }
+        }
+        display($song, $cover);
+    }
+    
+    function display($song, $cover){
+        echo "<img src='$cover.png' width='400' height='400'/>";
+        echo "<iframe width='560' height='400' src='$song' frameborder='0' allow='autoplay; encrypted-media' allowfullscreen></iframe>";
+    }
+    
+    function displayAlbums(){
+        $randomAlbums=array("1stRound","2001","2002","Aquemini","ATLiens");
+        foreach($randomAlbums as $ran){
+            echo "<img src='$ran.png' width='200' height='200'/>". "    ";
+        }
+    }
+?>
---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+    include 'inc/functions.php'
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <h1>Throwback Hit Songs</h1>
+        <br/>
+        <style>
+            @import url("css/styles.css");
+        </style>
+        <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
+    </head>
+    
+    <body>
+        <div id = "main">
+            <?php
+                Go();
+                echo "<br/>";
+            ?>
+        </div>
+    </body>
+    <br/>
+    <main align="center">
+        <br/>
+        <div id="playButton">
+            <form>
+                <button class="button" style="vertical-align:middle"><span>Hover </span></button>
+            </form>
+        </div>
+    </main>
+    <br/>
+    <h2>Other Related Albums</h2>
+    <footer align="center">
+        <?php
+            displayAlbums();
+        ?>
+    </footer>
+
+</html>
